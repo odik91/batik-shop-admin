c5e93b3e11c00c23639368b85e0847c731782b23
odik91 <ali.shoddiqien@gmail.com>
2023-09-28 17:49:28 +0700

    adding yajra datatable package

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -184,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'DataTables' => Yajra\DataTables\DataTablesServiceProvider::class,
     ])->toArray(),
 
 ];
